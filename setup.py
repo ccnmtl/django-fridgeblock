4dcacdc6af8d0ce0d14e9ecb9912fe4f2ae5da2c
Anders Pearson <anders@columbia.edu>
2014-05-12 10:21:25 -0400

    0.3.3, hopefully with template files properly packaged

--- setup.py
+++ setup.py
@@ -23,11 +23,11 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name="django-fridgeblock",
-    version="0.3.2",
+    version="0.3.3",
     author="Anders Pearson",
     author_email="anders@columbia.edu",
     url="",
