1c8402db3af9738083d1282d3bb3ee64fe261598
Anders Pearson <anders@columbia.edu>
2016-08-12 11:11:25 +0100

    :bookmark: release 0.5.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-fridgeblock",
-    version="0.4.0",
+    version="0.5.0",
     author="Anders Pearson",
     author_email="anders@columbia.edu",
     url="",
